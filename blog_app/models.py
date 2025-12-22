8770fd7de2ed98233b5079e82c539121de4417c2
Krischal-001 <your_email@example.comkrischallamichhane001@gmail.com>
2025-12-22 17:27:32 +0545

    Post list views is made

--- blog_app/models.py
+++ blog_app/models.py
@@ -16,8 +16,8 @@ class Post(models.Model):
     content=models.TextField()
     author=models.ForeignKey("auth.User",on_delete=models.CASCADE)
     created_at=models.DateTimeField(auto_now_add=True)
-    updated_at=models.DateTimeField(auto_now_add=True)
-    published_at=models.DateTimeField(null=True,balnk=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    published_at=models.DateTimeField(null=True,blank=True)
     
     def __str__(self):
         return self.title
