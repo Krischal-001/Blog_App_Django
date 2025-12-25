66e9101221a0fbbab75addc5eccaa8f4088060f3
Krischal-001 <your_email@example.comkrischallamichhane001@gmail.com>
2025-12-25 18:02:19 +0545

    form create and update has been completed successfully

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
-
-
+from django.shortcuts import render,redirect,get_object_or_404
+from blog_app.forms import PostForm
 from blog_app.models import Post
 from django.contrib.auth.decorators import login_required
 
@@ -36,10 +35,45 @@ def draft_detail(request,pk):
         "draft_detail.html",
         {"post":post}
     )
-def post_create(request,pk):
-    post=Post.objects.create(pk=pk)
+def post_create(request):
+    if request.method == "GET":
+     form=PostForm()
+     return render(
+         request,
+         "post_create.html",
+         {"form":form}
+     )
+    else :
+        form=PostForm(request.POST)
+        if form.is_valid():
+            post=form.save(commit=False)
+            post.author=request.user
+            post.save()
+            return redirect("draft-detail",pk=post.pk)
+        else:
+            return render(
+                request,
+                "post_create.html",
+                {"form":form}
+            ) 
+
+@login_required
+def post_update(request, pk):
+    post = get_object_or_404(Post, pk=pk, author=request.user)
+
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save()
+            if post.published_at:
+                return redirect("post-detail", post.pk)
+            else:
+                return redirect("draft-detail", post.pk)
+    else:
+        form = PostForm(instance=post)
+
     return render(
         request,
         "post_create.html",
-        {"post":post}
+        {"form": form}
     )
